9085bc5538eb781c98a4a3028a6f4a72a0d5c2c2
Ma1421 <mai08061286@gmail.com>
2023-01-12 18:59:40 +0900

    ブログ投稿の編集

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -19,6 +19,9 @@
                 <p class="body">{{ $post->body }}</p>
                 
             </div>
+        <div class="edit">
+            <a href="/posts/{{ $post->id }}/edit">edit</a>
+        </div>
         </div>
         <div class="footer">
             <a href="/">戻る</a>
