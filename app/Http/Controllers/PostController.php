bf89a0828bf812cf11d6c3bf76c8008d8d5c8aba
Ma1421 <mai08061286@gmail.com>
2023-01-17 19:49:27 +0900

    ‘9-2’

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,13 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\Category;
 
 class PostController extends Controller
 {
     public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
     {
         return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]);  
-       //blade内で使う変数'posts'と設定。'posts'の中身にgetを使いい、インスタンス化した$postを代入。
     }
+       //blade内で使う変数'posts'と設定。'posts'の中身にgetを使いい、インスタンス化した$postを代入
+       public function create(Category $category)
+{
+    return view('posts/create')->with(['categories' => $category->get()]);
+}
+    
 }
 ?>
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,4 +12,8 @@ class Post extends Model
     {
     return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
+    public function category()
+{
+    return $this->belongsTo(Category::class);
+}
 }
